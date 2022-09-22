aba0bddaa890d1d488ccb391832fd14dc3c318be
JBH01\jisamt4 <michael.torni@jbhunt.com>
2022-09-22 11:25:00 -0500

    add ngprime update theme

--- client/app-fox-creek/src/app/app.module.ts
+++ client/app-fox-creek/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app.routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
@@ -11,6 +15,7 @@ import { FooterComponent } from './footer/footer.component';
 import { EventService } from './services/event.service';
 import { GroupComponent } from './group/group.component';
 import { EventComponent } from './event/event.component';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +30,9 @@ import { EventComponent } from './event/event.component';
     CommonModule,
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    TableModule,
+    ButtonModule,
   ],
   providers: [EventService],
   bootstrap: [AppComponent],
--- client/app-fox-creek/src/app/event/event.component.ts
+++ client/app-fox-creek/src/app/event/event.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../services/event.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'fc-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.css'],
 })
 export class EventComponent implements OnInit {
   logo: string = './assets/images/logo.jpg';
-  logoAlt: string = 'FoodPlate logo';
+  logoAlt: string = 'Fox Creek Logo';
 
   events!: any;
   errorMessage!: string;
 
-  constructor(private eventService: EventService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private eventService: EventService) {}
 
   ngOnInit(): void {
     this.eventService.getEvents().subscribe({
@@ -30,5 +34,11 @@ export class EventComponent implements OnInit {
       },
     });
   }
-}
 
+  goToGroups(eventId: string): void{
+      this.router.navigate(["groups"], {
+        relativeTo: this.route,
+        queryParams: { event: `${eventId}` },
+      });
+  }
+}
